e4330388534f70a13924f371d31f4e4d52fd9f0d
taufeeque9 <9taufeeque9@gmail.com>
2024-07-31 14:42:21 -0700

    take asynchronous as an argument

--- cleanba/environments.py
+++ cleanba/environments.py
@@ -290,15 +290,14 @@ class AtariEnv(EnvpoolEnvConfig):
     reward_clip: bool = True
 
 
-def convert_to_cleanba_config(env_config):
-    """Converts an environment config from the learned_planner package to a cleanba environment config.
-    Asynchronous is set to False."""
+def convert_to_cleanba_config(env_config, asynchronous=False):
+    """Converts an environment config from the learned_planner package to a cleanba environment config."""
     if isinstance(env_config, EnvConfig):
         return env_config
     cls_name = env_config.__class__.__name__
     assert cls_name in globals(), f"{cls_name=} not available in cleanba.environments"
     args = dataclasses.asdict(env_config)
-    args["asynchronous"] = False
+    args["asynchronous"] = asynchronous
     args["num_envs"] = args.pop("n_envs")
     args.pop("n_envs_to_render", None)
     return globals()[cls_name](**args)
